47492bc493fd2d5c7af58283bbe7fe753e2d6f91
Ajeet <ajeetmdl23@gmail.com>
2023-01-31 23:34:20 -0500

    Update install.R

--- install.R
+++ install.R
@@ -9,8 +9,6 @@ BiocManager::install("SingleCellExperiment")
 install.packages("shiny")
 
 
-install.packages("ggplot2")
-
 install.packages("tidyverse")
 install.packages("rmarkdown")
 install.packages("httr")
